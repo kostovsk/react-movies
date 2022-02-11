6865023ad27c723d903dd8989a5af39e06b9a603
Stoycho Kostov <stoichokostov@gmail.com>
2022-02-10 22:32:30 -0600

    react fragment

--- src/Simple.tsx
+++ src/Simple.tsx
@@ -1,8 +1,8 @@
 export default function Simple(){
     return (
-        <div>
+        <>
             <h1>Hello World</h1>
             <h2>This is your world</h2>
-        </div>
+        </>
     )
 }
