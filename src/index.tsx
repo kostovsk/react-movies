2231a4579642009420435d6199e15981aabda760
Stoycho Kostov <stoichokostov@gmail.com>
2022-02-10 22:42:21 -0600

    expressions in jsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Simple from './Simple';
+import Expressions from './Expressions';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    <Simple />
+    <Expressions />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -17,3 +17,7 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//these were in dom render
+//<App />
+    //<Simple />
