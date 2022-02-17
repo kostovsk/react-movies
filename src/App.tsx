f045681bc1fef60f965619d9a72bff025c013939
Stoycho Kostov <stoichokostov@gmail.com>
2022-02-17 13:28:21 -0600

    useContext react hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import UseEffectExamples from './UseEffectExamples';
+import Grandparent from './Grandparent';
 import './App.css';
+import ValueContext from './ValueContext';
 
 // function component
 function App() {
@@ -10,7 +12,11 @@ function App() {
   return (
     <>
       <input type="checkbox" onChange={() => setHide(!hide)} /> Hide
-      {hide ? null : <UseEffectExamples />} 
+      {hide ? null : <UseEffectExamples />}
+
+      <ValueContext.Provider value={hide}>
+        <Grandparent />
+      </ValueContext.Provider>
     </>
   )
 }
