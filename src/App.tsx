43fa1168a2cfc9903f89c340e837c82f884fced7
Stoycho Kostov <stoichokostov@gmail.com>
2022-02-18 17:04:43 -0600

    movies list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,39 @@
 import './App.css';
-import IndividualMovie from './movies/IndividualMovie' 
+import IndividualMovie from './movies/IndividualMovie';
+import MoviesList from './movies/MoviesList';
 import { movieDTO } from './movies/movies.model';
 
 // function component
 function App() {
 
-  const testMovie: movieDTO = {
+  const inTheaters: movieDTO[] = [
+    {
     id: 1,
     title: 'Spinder man',
     poster: 'https://i.ytimg.com/vi/a4Mj8zwa32c/hqdefault.jpg',
-  }
-  
+    },
+    {
+    id: 2,
+    title: 'Gulub',
+    poster: 'https://pbs.twimg.com/profile_images/995928092534337536/m2ldkbFi_400x400.jpg',
+    }
+  ];
+
+  const upcomingReleases: movieDTO[] = [
+    {
+      id: 3,
+      title: 'Mafia',
+      poster: 'https://i.ytimg.com/vi/kaR1J6aNzK4/maxresdefault.jpg'
+    }
+  ]
+
   return (
     <>
-      <IndividualMovie {...testMovie} />
+      <h3>In Theaters</h3>
+      <MoviesList movies={inTheaters}/>
+
+      <h3>Upcoming Releases</h3>
+      <MoviesList movies={upcomingReleases} />
     </>
   )
 }
---
+++ src/movies/MoviesList.tsx
@@ -0,0 +1,16 @@
+import { movieDTO } from "./movies.model";
+import IndividualMovie from './IndividualMovie';
+import css from './MoviesList.module.css';
+
+export default function MoviesList(props: moviesListProps) {
+    return (
+        <div className={css.div}>
+            {props.movies.map(movie => 
+                <IndividualMovie {...movie} key={movie.id} />)}
+        </div>
+    )
+}
+
+interface moviesListProps{
+    movies: movieDTO[];
+}
