f43f62274e1e810971622677ea494255a037d586
Stoycho Kostov <stoichokostov@gmail.com>
2022-02-22 16:17:45 -0600

    making movie component more robust

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,48 @@
 import './App.css';
 import IndividualMovie from './movies/IndividualMovie';
 import MoviesList from './movies/MoviesList';
-import { movieDTO } from './movies/movies.model';
+import { landingPageDTO, movieDTO } from './movies/movies.model';
+import { useEffect, useState } from 'react';
 
 // function component
 function App() {
+  const [movies, setMovies] = useState<landingPageDTO>({});
 
-  const inTheaters: movieDTO[] = [
-    {
-    id: 1,
-    title: 'Spinder man',
-    poster: 'https://i.ytimg.com/vi/a4Mj8zwa32c/hqdefault.jpg',
-    },
-    {
-    id: 2,
-    title: 'Gulub',
-    poster: 'https://pbs.twimg.com/profile_images/995928092534337536/m2ldkbFi_400x400.jpg',
-    }
-  ];
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setMovies({
+        inTheaters: [
+          {
+            id: 1,
+            title: 'Spinder man',
+            poster: 'https://i.ytimg.com/vi/a4Mj8zwa32c/hqdefault.jpg',
+            },
+            {
+            id: 2,
+            title: 'Gulub',
+            poster: 'https://pbs.twimg.com/profile_images/995928092534337536/m2ldkbFi_400x400.jpg',
+            }
+        ],
+        upcomingReleases: [
+          {
+            id: 3,
+            title: 'Mafia',
+            poster: 'https://i.ytimg.com/vi/kaR1J6aNzK4/maxresdefault.jpg'
+          }
+        ]
+      })
+    }, 1000);
 
-  const upcomingReleases: movieDTO[] = [
-    {
-      id: 3,
-      title: 'Mafia',
-      poster: 'https://i.ytimg.com/vi/kaR1J6aNzK4/maxresdefault.jpg'
-    }
-  ]
+    return () => clearTimeout(timerId);
+  });
 
   return (
     <>
       <h3>In Theaters</h3>
-      <MoviesList movies={inTheaters}/>
+      <MoviesList movies={movies.inTheaters}/>
 
       <h3>Upcoming Releases</h3>
-      <MoviesList movies={upcomingReleases} />
+      <MoviesList movies={movies.upcomingReleases} />
     </>
   )
 }
---
+++ src/utils/Loading.tsx
@@ -0,0 +1,5 @@
+export default function Loading() {
+    return (
+        <img alt="loading" src='https://c.tenor.com/tEBoZu1ISJ8AAAAC/spinning-loading.gif' />
+    )
+}
