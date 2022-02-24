ef450360c083188cd91aa4eae07a6caf986faaf4
Stoycho Kostov <stoichokostov@gmail.com>
2022-02-24 00:18:06 -0600

    route parameters

--- src/genres/CreateGenre.tsx
+++ src/genres/CreateGenre.tsx
@@ -2,7 +2,9 @@ import { useHistory } from "react-router-dom";
 import Button from "../utils/Button";
 
 export default function CreateGenre() {
+
     const history = useHistory();
+    
     return (
         <>
             <h3>Create Genre</h3>
