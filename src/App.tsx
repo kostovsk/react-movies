9625561f2a7d4fae965a4e0cffd142f9228d091a
Stoycho Kostov <stoichokostov@gmail.com>
2022-02-23 22:52:37 -0600

    building menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import MoviesList from './movies/MoviesList';
+import Menu from './Menu';
 import { landingPageDTO } from './movies/movies.model';
 import { useEffect, useState } from 'react';
 
@@ -15,12 +16,12 @@ function App() {
             id: 1,
             title: 'Spinder man',
             poster: 'https://i.ytimg.com/vi/a4Mj8zwa32c/hqdefault.jpg',
-            },
-            {
+          },
+          {
             id: 2,
             title: 'Gulub',
             poster: 'https://pbs.twimg.com/profile_images/995928092534337536/m2ldkbFi_400x400.jpg',
-            }
+          }
         ],
         upcomingReleases: [
           {
@@ -36,13 +37,16 @@ function App() {
   });
 
   return (
-    <div className="container">
-      <h3>In Theaters</h3>
-      <MoviesList movies={movies.inTheaters}/>
+    <>
+      <Menu />
+      <div className="container">
+        <h3>In Theaters</h3>
+        <MoviesList movies={movies.inTheaters} />
 
-      <h3>Upcoming Releases</h3>
-      <MoviesList movies={movies.upcomingReleases} />
-    </div>
+        <h3>Upcoming Releases</h3>
+        <MoviesList movies={movies.upcomingReleases} />
+      </div>
+    </>
   )
 }
 
