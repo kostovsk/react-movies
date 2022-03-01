232368b182920b879366b3d78c9ad0244303f31a
Stoycho Kostov <stoichokostov@gmail.com>
2022-03-01 17:11:47 -0600

    create the actor form

--- src/actors/EditActor.tsx
+++ src/actors/EditActor.tsx
@@ -1,8 +1,15 @@
+import ActorForm from "./ActorForm";
+
 export default function EditActor() {
     return (
         <>
             <h3>Edit Actor</h3>
-
+            <ActorForm model={{
+                name: 'Tom Soyer',
+                dateOfBirth: new Date('1975-01-01T00:00:00')
+            }}
+                onSubmit={values => console.log(values)}
+            />
         </>
     )
 }
