6342f3f4554416bd700c4b4df4693a34725ed778
Stoycho Kostov <stoichokostov@gmail.com>
2022-03-07 16:35:41 -0600

    creating movie theater form

--- src/movietheaters/EditMovieTheaters.tsx
+++ src/movietheaters/EditMovieTheaters.tsx
@@ -1,8 +1,13 @@
+import MovieTheaterForm from './MovieTheaterForm';
+
 export default function EditMovieTheater() {
     return (
         <>
             <h3>Edit Movie Theater</h3>
-
+            <MovieTheaterForm
+                model={{ name: 'AMC' }}
+                onSubmit={values => console.log(values)}
+            />
         </>
     )
 }
