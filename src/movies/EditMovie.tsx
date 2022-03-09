5eae0e683079c29114ff71b365526ef73c8285dc
Stoycho Kostov <stoichokostov@gmail.com>
2022-03-08 18:01:29 -0600

    creating the movies form component

--- src/movies/EditMovie.tsx
+++ src/movies/EditMovie.tsx
@@ -1,8 +1,15 @@
+import MovieForm from './MovieForm';
+
 export default function EditMovie() {
     return (
         <>
             <h3>Edit Movie</h3>
-
+            <MovieForm model={{
+                title: 'Crank', inTheaters: true, trailer: 'url',
+                releaseDate: new Date('2022-01-01T00:00:00')
+            }}
+                onSubmit={values => (console.log(values))}
+            />
         </>
     )
 }
