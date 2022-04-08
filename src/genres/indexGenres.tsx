c4d36aa6595125731649e98dac1005cc290d0e2b
Stoycho Kostov <stoichokostov@gmail.com>
2022-04-08 17:10:50 -0500

    delete genres + sweetalert add

--- src/genres/indexGenres.tsx
+++ src/genres/indexGenres.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { urlGenres } from "../endpoints";
 import Button from "../utils/Button";
+import customConfirm from "../utils/customConfirm";
 import GenericList from "../utils/GenericList";
 import Pagination from "../utils/Pagination";
 import RecordsPerPageSelect from "../utils/RecordsPerPageSelect";
@@ -16,6 +17,11 @@ export default function IndexGenres() {
     const [page, setPage] = useState(1);
 
     useEffect(() => {
+        loadData();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [page, recordsPerPage])
+
+    function loadData() {
         axios.get(urlGenres, {
             params: { page, recordsPerPage }
         })
@@ -25,7 +31,19 @@ export default function IndexGenres() {
                 setTotalAmountOfPages(Math.ceil(totalAmountOfRecords / recordsPerPage));
                 setGenres(response.data);
             })
-    }, [page, recordsPerPage])
+    }
+
+    async function deleteGenre(id: number) {
+        try {
+            await axios.delete(`${urlGenres}/${id}`);
+            loadData();
+        }
+        catch (error) {
+            if (error && error.response) {
+                console.error(error.response.data);
+            }
+        }
+    }
 
     return (
         <>
@@ -56,7 +74,9 @@ export default function IndexGenres() {
                                     <Link className="btn btn-success"
                                         to={`/genres/edit/${genre.id}`}>Edit</Link>
 
-                                    <Button className="btn btn-danger">Delete</Button>
+                                    <Button
+                                        onClick={() => customConfirm(() => deleteGenre(genre.id))}
+                                        className="btn btn-danger">Delete</Button>
                                 </td>
                                 <td>
                                     {genre.name}
