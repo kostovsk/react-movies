d89dbe0cfa3c3f22c749fe48d8e043499f4bca3c
Stoycho Kostov <stoichokostov@gmail.com>
2022-04-21 16:56:16 -0500

    add edit entity to editmovietheater

--- src/movietheaters/EditMovieTheaters.tsx
+++ src/movietheaters/EditMovieTheaters.tsx
@@ -1,17 +1,18 @@
+import { urlMovieTheaters } from '../endpoints';
+import EditEntity from '../utils/EditEntity';
+import { movieTheaterCreationDTO, movieTheaterDTO } from './movieTheater.mode';
 import MovieTheaterForm from './MovieTheaterForm';
 
 export default function EditMovieTheater() {
     return (
-        <>
-            <h3>Edit Movie Theater</h3>
-            <MovieTheaterForm
-                model={{
-                    name: 'AMC',
-                    latitude: 42.06245458505791,
-                    longitude: -88.01362037658691
-                }}
-                onSubmit={values => console.log(values)}
-            />
-        </>
+        <EditEntity<movieTheaterCreationDTO, movieTheaterDTO>
+            url={urlMovieTheaters} indexURL="/movietheaters" entityName="Movie Theater"
+        >
+            {(entity, edit) =>
+                <MovieTheaterForm model={entity}
+                    onSubmit={async values => await edit(values)}
+                />
+            }
+        </EditEntity>
     )
 }
