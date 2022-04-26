e22444cc547a966b831f952969f4bfe8886e7a19
Stoycho Kostov <stoichokostov@gmail.com>
2022-04-26 18:39:09 -0500

    adding logic to make a get request to back end

--- src/movies/CreateMovie.tsx
+++ src/movies/CreateMovie.tsx
@@ -1,25 +1,40 @@
+import axios, { AxiosResponse } from 'axios';
+import { useEffect, useState } from 'react';
+import { urlMovies } from '../endpoints';
 import { genreDTO } from '../genres/genres.model';
 import { movieTheaterDTO } from '../movietheaters/movieTheater.mode';
+import Loading from '../utils/Loading';
 import MovieForm from './MovieForm';
+import { moviesPostGetDTO } from './movies.model';
 
 export default function CreateMovie() {
 
-    const nonSelectedGenres: genreDTO[] = [{ id: 1, name: 'Comedy' }, { id: 2, name: 'Drama' }]
-    const nonSelectedMovieTheaters: movieTheaterDTO[]
-        = [{ id: 1, name: 'AMC' }, { id: 2, name: 'TEX' }]
+    const [nonSelectedGenres, setNonSelectedGenres] = useState<genreDTO[]>([]);
+    const [nonSelectedMovieTheaters, setNonSelectedMovieTheaters] = useState<movieTheaterDTO[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        axios.get(`${urlMovies}/PostGet`)
+            .then((response: AxiosResponse<moviesPostGetDTO>) => {
+                setNonSelectedGenres(response.data.genres);
+                setNonSelectedMovieTheaters(response.data.movieTheaters);
+                setLoading(false);
+            })
+    }, [])
 
     return (
         <>
             <h3>Create Movie</h3>
-            <MovieForm model={{ title: '', inTheaters: false, trailer: '' }}
-                onSubmit={values => (console.log(values))}
-                nonSelectedGenres={nonSelectedGenres}
-                selectedGenres={[]}
+            {loading ? <Loading /> :
+                <MovieForm model={{ title: '', inTheaters: false, trailer: '' }}
+                    onSubmit={values => (console.log(values))}
+                    nonSelectedGenres={nonSelectedGenres}
+                    selectedGenres={[]}
 
-                nonSelectedMovieTheaters={nonSelectedMovieTheaters}
-                selectedMovieTheaters={[]}
-                selectedActors={[]}
-            />
+                    nonSelectedMovieTheaters={nonSelectedMovieTheaters}
+                    selectedMovieTheaters={[]}
+                    selectedActors={[]}
+                />}
         </>
     )
 }
