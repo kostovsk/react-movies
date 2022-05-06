1c9c3710e328c455900bcd8382d9f7f2f0a16e1c
Stoycho Kostov <stoichokostov@gmail.com>
2022-05-06 17:03:29 -0500

    movie details

--- src/movies/LandingPage.tsx
+++ src/movies/LandingPage.tsx
@@ -6,32 +6,8 @@ export default function LandingPage() {
     const [movies, setMovies] = useState<landingPageDTO>({});
 
     useEffect(() => {
-        const timerId = setTimeout(() => {
-            setMovies({
-                inTheaters: [
-                    {
-                        id: 1,
-                        title: 'Spinder man',
-                        poster: 'https://i.ytimg.com/vi/a4Mj8zwa32c/hqdefault.jpg',
-                    },
-                    {
-                        id: 2,
-                        title: 'Gulub',
-                        poster: 'https://pbs.twimg.com/profile_images/995928092534337536/m2ldkbFi_400x400.jpg',
-                    }
-                ],
-                upcomingReleases: [
-                    {
-                        id: 3,
-                        title: 'Mafia',
-                        poster: 'https://i.ytimg.com/vi/kaR1J6aNzK4/maxresdefault.jpg'
-                    }
-                ]
-            })
-        }, 1000);
 
-        return () => clearTimeout(timerId);
-    });
+    }, []);
 
     return (
         <>
